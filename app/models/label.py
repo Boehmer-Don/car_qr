0d065a2ea1e2afade5cf9a3c77b936ff2f1a843b
denys <denysburimov@gmai.com>
2023-07-18 19:52:11 +0300

    label architechture

---
+++ app/models/label.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+import enum
+from uuid import uuid4
+import sqlalchemy as sa
+from sqlalchemy import orm
+from sqlalchemy.ext.hybrid import hybrid_property
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app.database import db
+from .utils import ModelMixin
+from app.logger import log
+from app import schema as s
+
+
+def gen_label_unique_id() -> str:
+    return str(uuid4())
+
+
+class Label(db.Model, ModelMixin):
+    __tablename__ = "labels"
+
+    """
+    Label fields:
+    id
+    unique_id
+    name
+    make
+    vehicle_model
+    year
+    mileage
+    color
+    trim
+    type_of_vehicle
+    price
+    date_received
+    url
+
+    """
+
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    name: orm.Mapped[str] = orm.mapped_column(sa.String(64), default="")
+    make: orm.Mapped[str] = orm.mapped_column(sa.String(64), default="")
+    unique_id: orm.Mapped[str] = orm.mapped_column(
+        sa.String(36),
+        default=gen_label_unique_id,
+    )
+    created_at: orm.Mapped[datetime] = orm.mapped_column(
+        sa.DateTime,
+        default=datetime.utcnow,
+    )
+    activated: orm.Mapped[bool] = orm.mapped_column(sa.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<{self.id}:{self.name}>"
+
+    @property
+    def json(self):
+        u = s.User.from_orm(self)
+        return u.json()
---
+++ app/views/label.py
@@ -0,0 +1,84 @@
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for,
+)
+from flask_login import login_required, current_user
+import sqlalchemy as sa
+from flask import current_app as app
+from app.controllers import create_pagination
+from app import models as m, db
+from app import forms as f
+from app.logger import log
+
+
+dealer_blueprint = Blueprint("labels", __name__, url_prefix="/labels")
+
+
+@dealer_blueprint.route("/active", methods=["GET"])
+@login_required
+def get_active_labels():
+    ...
+
+
+@dealer_blueprint.route("/archived", methods=["GET"])
+@login_required
+def get_archived_labels():
+    ...
+
+
+@dealer_blueprint.route("/deactivate/<label_unique_id>", methods=["GET", "POST"])
+@login_required
+def deactivate_label(label_unique_id: str):
+    ...
+
+
+@dealer_blueprint.route("/info", methods=["GET"])
+@login_required
+def label_details():
+    ...
+
+
+@dealer_blueprint.route("/amount", methods=["GET", "POST"])
+@login_required
+def new_label_set_amount():
+    ...
+
+
+@dealer_blueprint.route("/details/<label_unique_id>", methods=["GET", "POST"])
+@login_required
+def new_label_set_details(label_unique_id: str):
+    """
+    Label fields:
+    id
+    label_unique_id
+    name
+    make
+    vehicle_model
+    year
+    mileage
+    color
+    trim
+    type_of_vehicle
+    price
+    date_received
+    url
+    views
+
+    """
+    ...
+
+
+@dealer_blueprint.route("/payment/<label_unique_id>", methods=["GET", "POST"])
+@login_required
+def new_label_payment(label_unique_id: str):
+    ...
+
+
+@dealer_blueprint.route("/l/<label_unique_id>")
+def redirect_to_outer_url(label_unique_id: str):
+    # Counter
+    return redirect(url_outer)
