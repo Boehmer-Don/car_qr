30776122eb7ef7454460474b81046cbbf8937433
denys <denysburimov@gmai.com>
2023-07-20 16:13:55 +0300

    label response

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -3,8 +3,10 @@ from tests.utils import login
 
 
 def test_labels_list(populate: FlaskClient):
-    # login(populate)
-    response = populate.get("/labels/active", follow_redirects=True)
+    login(populate)
+    response = populate.get("/labels/active")
+    assert response
     assert response.status_code == 200
-    assert b"Active labels" in response.data
+    assert b"Active Labels" in response.data
     assert b"Welcome Back" in response.data
+    # assert b"My Account" in response.data
