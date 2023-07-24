6f441e6ee4e2bd2e7a7fc0440c65bd4a5c370941
denys <denysburimov@gmai.com>
2023-07-24 14:45:26 +0300

    archived labels

--- app/controllers/jinja_globals.py
+++ app/controllers/jinja_globals.py
@@ -10,3 +10,7 @@ def form_hidden_tag():
 
 def time_delta(created_at: datetime):
     return (datetime.utcnow() - created_at).days
+
+
+def days_active(date_received: datetime, date_deactivated: datetime):
+    return (date_deactivated - date_received).days
