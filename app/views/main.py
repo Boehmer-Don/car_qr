6eb1440a8ad5a5da6f27633da2f81999f68eeae0
denys <denysburimov@gmai.com>
2023-08-23 10:21:06 +0300

    gift route fix

--- app/views/main.py
+++ app/views/main.py
@@ -23,7 +23,7 @@ def redirect_to_outer_url(sticker_id: str):
 
     user: m.User = db.session.scalar(m.User.select().where(m.User.id == label.user_id))
 
-    if user and user.gift_enabled:
+    if user and user.gift:
         return redirect(url_for("user.gift", sticker_id=sticker_id))
         # Create user's gift page
 
