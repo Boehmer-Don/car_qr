b53e8299bf0ccebf9e0c19805a0deb3ee95ab495
denys <denysburimov@gmai.com>
2023-08-29 09:09:59 +0300

    mobile fixes

--- src/custom.ts
+++ src/custom.ts
@@ -139,3 +139,81 @@ secondArrowButtons.forEach(secondArrowButton => {
     });
   });
 });
+
+// Select the elements to reveal
+const revealElements = document.querySelectorAll('.reveal-element');
+
+// Create an Intersection Observer instance
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (entry.target.classList.contains('qr-code')) {
+          entry.target.classList.add('flipInY');
+          entry.target.classList.add('animated');
+          entry.target.classList.add('reveal'); // Add the "reveal" class to trigger the animation
+        } else {
+          entry.target.classList.add('reveal'); // Add the "reveal" class to trigger the animation
+          observer.unobserve(entry.target); // Stop observing once revealed
+        }
+      }
+    });
+  },
+  {threshold: 0.5},
+); // Adjust the threshold as needed
+
+// Start observing each reveal element
+revealElements.forEach(element => {
+  observer.observe(element);
+});
+
+const landing_mobile_menu_button: HTMLButtonElement = document.querySelector(
+  '#landing_mobile_menu_button',
+);
+const landing_mobile_menu: HTMLDivElement = document.querySelector(
+  '#landing_mobile_menu',
+);
+const footer: HTMLElement = document.querySelector('footer');
+const sections: NodeListOf<HTMLElement> = document.querySelectorAll('section');
+if (landing_mobile_menu_button) {
+  landing_mobile_menu_button.addEventListener('click', () => {
+    landing_mobile_menu.classList.toggle('hidden');
+    footer.classList.toggle('hidden');
+    sections.forEach(section => {
+      section.classList.toggle('hidden');
+    });
+  });
+}
+
+const mobile_pricing: HTMLDivElement =
+  document.querySelector('#mobile_pricing');
+const mobile_contacts: HTMLDivElement =
+  document.querySelector('#mobile_contact');
+console.log(mobile_contacts);
+
+if (mobile_pricing) {
+  mobile_pricing.addEventListener('click', () => {
+    landing_mobile_menu.classList.toggle('hidden');
+    footer.classList.toggle('hidden');
+    sections.forEach(section => {
+      section.classList.toggle('hidden');
+    });
+    document.querySelector('#pricing').scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+}
+
+if (mobile_contacts) {
+  console.log(mobile_contacts);
+  mobile_contacts.addEventListener('click', () => {
+    landing_mobile_menu.classList.toggle('hidden');
+    footer.classList.toggle('hidden');
+    sections.forEach(section => {
+      section.classList.toggle('hidden');
+    });
+    document.querySelector('#contacts').scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+}
