2232010576197b2b2cab463e5f8fe658e0a0b9bb
denys <denysburimov@gmai.com>
2023-09-06 12:35:03 +0300

    working commit

--- app/static/js/new_label.d.ts
+++ app/static/js/new_label.d.ts
@@ -1,3 +1,4 @@
+declare const suggestionContainers: NodeListOf<HTMLDivElement>;
 declare const makeContainer: HTMLDivElement;
 declare const makeInput: HTMLInputElement;
 declare const makeSuggestionP: HTMLParagraphElement;
--- src/new_label.ts
+++ src/new_label.ts
@@ -1,5 +1,17 @@
 console.log('new_label.ts');
 
+const suggestionContainers: NodeListOf<HTMLDivElement> =
+  document.querySelectorAll('.suggestion-container');
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    console.log('Escape pressed');
+    suggestionContainers.forEach(container => {
+      container.classList.add('hidden');
+    });
+  }
+});
+
 const makeContainer: HTMLDivElement = document.querySelector('.make-container');
 const makeInput: HTMLInputElement = document.querySelector('#make-1');
 const makeSuggestionP: HTMLParagraphElement =
@@ -7,7 +19,6 @@ const makeSuggestionP: HTMLParagraphElement =
 
 if (makeInput) {
   makeInput.addEventListener('input', e => {
-    console.log('makeInput changed');
     makeContainer.classList.remove('hidden');
     let clonedMakeSuggestionParagraph: HTMLParagraphElement =
       makeSuggestionP.cloneNode(true) as HTMLParagraphElement;
