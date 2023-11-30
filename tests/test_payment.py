2cae00b5cae0b6a48162d6467a978fe6dbb016ad
BohdanSizomin <bohdansizomin@gmail.com>
2023-11-30 16:26:39 +0200

    tests

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -1,6 +1,8 @@
 from flask.testing import FlaskClient
+import pytest
 
 
+@pytest.skip("Skipping payment tests", allow_module_level=True)
 def test_webhook(client: FlaskClient, monkeypatch):
     headers = {
         "STRIPE_SIGNATURE": "test_stripe_signature",
