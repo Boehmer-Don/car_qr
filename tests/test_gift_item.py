c1870c75e3a76fe022f32c6c307b0739551acb60
nazarkobryn <kobrinnazar05@gmail.com>
2024-09-02 14:24:24 -0400

    udate test

--- tests/test_gift_item.py
+++ tests/test_gift_item.py
@@ -1,8 +1,14 @@
+import io
+from pathlib import Path
 import sqlalchemy as sa
 from flask.testing import FlaskClient
 
 from app import models as m, db
 from tests.utils import set_user
+
+
+test_image = "test.jpg"
+test_image_path = Path("tests") / "data" / test_image
 
 
 test_gift_box = {
@@ -33,7 +39,8 @@ def test_access(client: FlaskClient):
     assert res.status_code == 200
 
 
-def test_gift_items_CRUD(client: FlaskClient):
+def test_gift_items_CRUD(client: FlaskClient, mocker):
+    mocker.patch("app.views.gift_items.save_file", return_value="test.jpg")
 
     set_user(client, role=m.UsersRole.admin)
 
@@ -41,6 +48,10 @@ def test_gift_items_CRUD(client: FlaskClient):
     assert res.status_code == 200
     assert b"Add Gift Item" in res.data
 
+    with open(test_image_path, "rb") as f:
+        file = (io.BytesIO(f.read()), test_image)
+
+    test_gift_box["image"] = file
     res = client.post(
         "/gift-items/",
         data=test_gift_box,
@@ -67,6 +78,7 @@ def test_gift_items_CRUD(client: FlaskClient):
     test_gift_box["description"] = "Updated"
     test_gift_box["price"] = 200.0
     test_gift_box["gift_item_unique_id"] = gift_item.unique_id
+    del test_gift_box["image"]
 
     res = client.post(
         "/gift-items/edit",
@@ -77,7 +89,9 @@ def test_gift_items_CRUD(client: FlaskClient):
     assert b"Gift Item updated successfully" in res.data
     assert gift_item.description == "Updated"
     assert gift_item.price == 200.0
+    assert gift_item.image_path == test_image
 
+    # we don't have this logic yet
     # res = client.delete(f"/gift-items/{gift_item.id}/delete")
     # assert res.status_code == 200
     # assert b"Gift Item deleted successfully" in res.data
