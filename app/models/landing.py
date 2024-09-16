b6162922cf250679868fb5a69bd163f3b994ffa1
nazarkobryn <kobrinnazar05@gmail.com>
2024-09-16 10:25:04 -0400

    add captcha

--- app/models/landing.py
+++ app/models/landing.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 
@@ -7,4 +7,5 @@ class LandingForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     message = StringField("Message", validators=[DataRequired()])
+    recaptcha = RecaptchaField()
     submit = SubmitField("Submit")
