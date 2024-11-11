9b691f572bab56a29ba2e8766b012d00b33f37b5
Kostia Stoliarskiy <saintkos117@gmail.com>
2024-11-11 17:50:19 +0200

    fix

--- app/controllers/subscriptions_expiration_check.py
+++ app/controllers/subscriptions_expiration_check.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 import sqlalchemy as sa
-from flask import render_template, current_app as app
+from flask import render_template, current_app as app, url_for
 from flask_mail import Message, Mail
 from app import models as m, db
 from app.logger import log
 from .utils import custom_url_for
 
 mail = Mail()
+
+SUBSCRIPTION_EXPIRATION_BREAKPOINT_DAYS = [1, 7, 30]
 
 
 def check_subscriptions():
@@ -23,14 +25,18 @@ def check_subscriptions():
             datetime.fromtimestamp(user.subscriptions[0].current_period_end)
             - datetime.now()
         ).days
-        if 0 < days_left < 21:
+        if days_left in SUBSCRIPTION_EXPIRATION_BREAKPOINT_DAYS:
             msg = Message(
                 subject="New Customer",
                 sender=app.config["MAIL_DEFAULT_SENDER"],
                 recipients=[user.email],
             )
-
-            msg.html = render_template("email/expiration_notification.htm")
+            subs_link = custom_url_for(
+                url_for("auth.plan", user_unique_id=user.unique_id)
+            )
+            msg.html = render_template(
+                "email/expiration_notification.htm", subs_link=subs_link
+            )
             log(
                 log.INFO,
                 "User [%s], subscription expires in [%s] days",
@@ -41,6 +47,7 @@ def check_subscriptions():
         elif days_left == 0:
             user.activated = False
             user.save()
+            user.is_authenticated = False
             msg = Message(
                 subject="Subscription renewal",
                 sender=app.config["MAIL_DEFAULT_SENDER"],
